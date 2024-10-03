275b91e02fc37a606a764162fd58bb0bb2ff1d0c
sipsandsparks <admin@sipsandsparks.org>
2024-10-03 15:27:42 -0400

    Update SuperAdminPage.vue

--- src/components/Admin/SuperAdminPage.vue
+++ src/components/Admin/SuperAdminPage.vue
@@ -33,13 +33,8 @@ window.addEventListener('resize', () => {
   windowWidth.value = window.innerWidth;
 });
 
-const isTablet = computed(() => {
-  return windowWidth.value >= 450;
-});
-
-const isBigTablet = computed(() => {
-  return windowWidth.value >= 639;
-});
+const isTablet = computed(() => windowWidth.value >= 450);
+const isBigTablet = computed(() => windowWidth.value >= 639);
 
 const toastVisible = ref(false);
 const toastFadeout = ref(false);
@@ -53,49 +48,46 @@ const continueToAdminForm = ref(false);
 const participants = ref<EventParticipant[]>([]);
 const adminParticipants = ref<AdminEventParticipant[]>([]);
 
-const loadParticipants = async () => {
-    try {
-        // Make the POST request to the backend server
-        const response = await axios.post('https://sips-and-sparks-77e98866d158.herokuapp.com/admin/event-participants', {
-            username: username.value,
-            password: password.value,
-            eventId: selectedEventId.value
-        });
+// Helper to show toast notifications
+const showToast = (message: string, duration: number = 4000) => {
+  toastMessage.value = message;
+  toastVisible.value = true;
+  toastFadeout.value = false;
 
-        if (response.data.error) {
-            toastMessage.value = response.data.error;
-            toastVisible.value = true;
-        } else {
-            participants.value = response.data.public_attendees;
-            adminParticipants.value = response.data.admin_attendees;
-            toastMessage.value = 'Success!'
-            return;
-        }
-    
-        setTimeout(() => {
-            toastFadeout.value = true;
-        }, 3000);
-        setTimeout(() => {
-            toastVisible.value = false;
-            toastFadeout.value = false;
-        }, 4000);
-    } catch (error) {
-        toastMessage.value = 'Error. Please try again later or contact us.';
-        toastVisible.value = true;
-        setTimeout(() => {
-            toastFadeout.value = true;
-        }, 3000);
-        setTimeout(() => {
-            toastVisible.value = false;
-            toastFadeout.value = false;
-        }, 3500);
+  setTimeout(() => {
+    toastFadeout.value = true;
+  }, duration - 1000);
+
+  setTimeout(() => {
+    toastVisible.value = false;
+    toastFadeout.value = false;
+  }, duration);
+};
+
+const loadParticipants = async () => {
+  try {
+    const response = await axios.post('https://sips-and-sparks-77e98866d158.herokuapp.com/admin/event-participants', {
+      username: username.value,
+      password: password.value,
+      eventId: selectedEventId.value
+    });
+
+    if (response.data.error) {
+      showToast(response.data.error);
+    } else {
+      participants.value = response.data.public_attendees;
+      adminParticipants.value = response.data.admin_attendees;
+      showToast('Success!');
     }
+  } catch (error) {
+    showToast('Error. Please try again later or contact us.');
+  }
 };
 
 const continueToEventSelection = (events: Event[], user: string, pass: string) => {
-    username.value = user;
-    password.value = pass;
-    availableEvents.value = events;
+  username.value = user;
+  password.value = pass;
+  availableEvents.value = events;
 };
 
 const continueToForm = (eventId: string) => {
@@ -103,81 +95,60 @@ const continueToForm = (eventId: string) => {
 };
 
 const continueToView = async (viewNormal: boolean) => {
-    if (viewNormal) {
-        continueToAdminForm.value = false;
-        await loadParticipants();
-        continueToNormalForm.value = true;
-    } else {
-        continueToNormalForm.value = false;
-        await loadParticipants();
-        continueToAdminForm.value = true;
-    }
-}
+  if (viewNormal) {
+    continueToAdminForm.value = false;
+    await loadParticipants();
+    continueToNormalForm.value = true;
+  } else {
+    continueToNormalForm.value = false;
+    await loadParticipants();
+    continueToAdminForm.value = true;
+  }
+};
 </script>
 
 <template>
-    <div>
-        <AdminLogin v-if="availableEvents.length === 0" :is-tablet="isBigTablet" @continue="continueToEventSelection" />
-        <AdminEventSelection v-else-if="availableEvents.length > 0 && !selectedEventId" :is-tablet="isTablet" :events="availableEvents" @continue="continueToForm" />
-        <AdminChooseView v-else-if="selectedEventId && !continueToAdminForm && !continueToNormalForm" :is-tablet="isBigTablet" @continue="continueToView" />
-        <MatchForm v-else-if="selectedEventId && continueToNormalForm && participants.length > 0" :is-tablet="isTablet" :participants="participants" :event-id="selectedEventId" :first-name="''" :last-name="''" :email="''" :previous-info="undefined" :disable-submit="true" />
-        <HostPage v-else-if="selectedEventId && continueToAdminForm && adminParticipants.length > 0" :is-tablet="isTablet" :participants="adminParticipants" :event-id="selectedEventId" :first-name="''" :last-name="''" :email="''" :disable-refresh="true" />
-        <div id="snackbar" :class="{show: toastVisible, fadeout: toastFadeout}">{{ toastMessage }}</div>
-    </div>
+  <div>
+    <AdminLogin v-if="availableEvents.length === 0" :is-tablet="isBigTablet" @continue="continueToEventSelection" />
+    <AdminEventSelection v-else-if="availableEvents.length > 0 && !selectedEventId" :is-tablet="isTablet" :events="availableEvents" @continue="continueToForm" />
+    <AdminChooseView v-else-if="selectedEventId && !continueToAdminForm && !continueToNormalForm" :is-tablet="isBigTablet" @continue="continueToView" />
+    <MatchForm v-else-if="selectedEventId && continueToNormalForm && participants.length > 0" :is-tablet="isTablet" :participants="participants" :event-id="selectedEventId" :first-name="''" :last-name="''" :email="''" :previous-info="undefined" :disable-submit="true" />
+    <HostPage v-else-if="selectedEventId && continueToAdminForm && adminParticipants.length > 0" :is-tablet="isTablet" :participants="adminParticipants" :event-id="selectedEventId" :first-name="''" :last-name="''" :email="''" :disable-refresh="true" />
+    <div id="snackbar" :class="{ show: toastVisible, fadeout: toastFadeout }">{{ toastMessage }}</div>
+  </div>
 </template>
 
 <style scoped>
-/* The snackbar - position it at the bottom and in the middle of the screen */
 #snackbar {
-    visibility: hidden;
-    min-width: 250px;
-    left: 50%;
-    transform: translateX(-50%);
-    background-color: #333;
-    color: #fff;
-    text-align: center;
-    border-radius: 2px;
-    padding: 16px;
-    position: fixed;
-    z-index: 1;
-    bottom: 10vh;
-    font-family: "Darker Grotesque", sans-serif;
-    font-optical-sizing: auto;
-    font-weight: 650;
-    font-style: normal;
-    font-size: 1.25rem;
-}
-    
-/* Show the snackbar when clicking on a button (class added with JavaScript) */
-#snackbar.show {
-    visibility: visible; /* Show the snackbar */
-    -webkit-animation: fadein 0.5s;
-    animation: fadein 0.5s;
+  visibility: hidden;
+  min-width: 250px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: #333;
+  color: #fff;
+  text-align: center;
+  border-radius: 2px;
+  padding: 16px;
+  position: fixed;
+  z-index: 1;
+  bottom: 10vh;
+  font-family: "Darker Grotesque", sans-serif;
+  font-weight: 650;
+  font-size: 1.25rem;
 }
 
-#snackbar.fadeout {
-    -webkit-animation: fadeout 0.5s forwards;
-    animation: fadeout 0.5s forwards;
+#snackbar.show {
+  visibility: visible;
+  animation: fadein 0.5s, fadeout 0.5s 2.5s;
 }
-    
-/* Animations to fade the snackbar in and out */
-@-webkit-keyframes fadein {
-    from {bottom: 0; opacity: 0;}
-    to {bottom: 10vh; opacity: 1;}
-}
-    
+
 @keyframes fadein {
-    from {bottom: 0; opacity: 0;}
-    to {bottom: 10vh; opacity: 1;}
+  from { bottom: 0; opacity: 0; }
+  to { bottom: 10vh; opacity: 1; }
 }
-    
-@-webkit-keyframes fadeout {
-    from {bottom: 10vh; opacity: 1;}
-    to {bottom: 0; opacity: 0;}
-}
-    
+
 @keyframes fadeout {
-    from {bottom: 10vh; opacity: 1;}
-    to {bottom: 0; opacity: 0;}
+  from { bottom: 10vh; opacity: 1; }
+  to { bottom: 0; opacity: 0; }
 }
 </style>
