bc0ea021ab4e4d07fb5b8f248f2b0a0cfb598bff
sipsandsparks <admin@sipsandsparks.org>
2024-10-08 13:08:59 -0400

    Update SuperAdminPage.vue

--- src/components/Admin/SuperAdminPage.vue
+++ src/components/Admin/SuperAdminPage.vue
@@ -114,9 +114,135 @@ const continueToView = async (viewNormal: boolean) => {
     <AdminChooseView v-else-if="selectedEventId && !continueToAdminForm && !continueToNormalForm" :is-tablet="isBigTablet" @continue="continueToView" />
     <MatchForm v-else-if="selectedEventId && continueToNormalForm && participants.length > 0" :is-tablet="isTablet" :participants="participants" :event-id="selectedEventId" :first-name="''" :last-name="''" :email="''" :previous-info="undefined" :disable-submit="true" />
     <HostPage v-else-if="selectedEventId && continueToAdminForm && adminParticipants.length > 0" :is-tablet="isTablet" :participants="adminParticipants" :event-id="selectedEventId" :first-name="''" :last-name="''" :email="''" :disable-refresh="true" />
+    
+    <!-- Add your checkbox for no-show management -->
+    <div v-for="participant in adminParticipants" :key="participant.email">
+      <label>
+        <input type="checkbox" v-model="noShowCheckedItems[participant.email]" />
+        No Show
+      </label>
+    </div>
+
     <div id="snackbar" :class="{ show: toastVisible, fadeout: toastFadeout }">{{ toastMessage }}</div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { computed, Ref, ref } from 'vue';
+import axios from 'axios';
+import AdminLogin from './AdminLogin.vue';
+import AdminEventSelection from './AdminEventSelection.vue';
+import AdminChooseView from './AdminChooseView.vue';
+import MatchForm from '../Match/MatchForm.vue';
+import HostPage from '../Match/HostPage.vue';
+
+export interface EventParticipant {
+    id: string;
+    name: string;
+}
+
+export interface AdminEventParticipant {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  gender: string;
+  inAttendance: boolean;
+  noShow?: boolean;  // Add noShow attribute
+}
+
+export interface Event {
+    id: string;
+    name: string;
+    date: Date;
+}
+
+const windowWidth = ref(window.innerWidth);
+
+window.addEventListener('resize', () => {
+  windowWidth.value = window.innerWidth;
+});
+
+const isTablet = computed(() => windowWidth.value >= 450);
+const isBigTablet = computed(() => windowWidth.value >= 639);
+
+const toastVisible = ref(false);
+const toastFadeout = ref(false);
+const toastMessage = ref('Loading...');
+const selectedEventId: Ref<string | undefined> = ref(undefined);
+const availableEvents = ref<Event[]>([]);
+const username = ref('');
+const password = ref('');
+const continueToNormalForm = ref(false);
+const continueToAdminForm = ref(false);
+const participants = ref<EventParticipant[]>([]);
+const adminParticipants = ref<AdminEventParticipant[]>([]);
+const noShowCheckedItems = ref({});  // Add ref for checkbox
+
+// Helper to show toast notifications
+const showToast = (message: string, duration: number = 4000) => {
+  toastMessage.value = message;
+  toastVisible.value = true;
+  toastFadeout.value = false;
+
+  setTimeout(() => {
+    toastFadeout.value = true;
+  }, duration - 1000);
+
+  setTimeout(() => {
+    toastVisible.value = false;
+    toastFadeout.value = false;
+  }, duration);
+};
+
+const loadParticipants = async () => {
+  try {
+    const response = await axios.post('https://sips-and-sparks-77e98866d158.herokuapp.com/admin/event-participants', {
+      username: username.value,
+      password: password.value,
+      eventId: selectedEventId.value
+    });
+
+    if (response.data.error) {
+      showToast(response.data.error);
+    } else {
+      participants.value = response.data.public_attendees;
+      adminParticipants.value = response.data.admin_attendees;
+      adminParticipants.value.forEach((participant) => {
+        noShowCheckedItems.value = {
+          ...noShowCheckedItems.value,
+          [participant.email]: participant.noShow || false,
+        };
+      });
+      showToast('Success!');
+    }
+  } catch (error) {
+    showToast('Error. Please try again later or contact us.');
+  }
+};
+
+const continueToEventSelection = (events: Event[], user: string, pass: string) => {
+  username.value = user;
+  password.value = pass;
+  availableEvents.value = events;
+};
+
+const continueToForm = (eventId: string) => {
+  selectedEventId.value = eventId;
+};
+
+const continueToView = async (viewNormal: boolean) => {
+  if (viewNormal) {
+    continueToAdminForm.value = false;
+    await loadParticipants();
+    continueToNormalForm.value = true;
+  } else {
+    continueToNormalForm.value = false;
+    await loadParticipants();
+    continueToAdminForm.value = true;
+  }
+};
+</script>
 
 <style scoped>
 #snackbar {
